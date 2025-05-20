0de214dd0128a9a1470e67c54801d8d5821842bb
Valeria Saidid Miranda Ibarra <valeria.miranda@utec.edu.pe>
2025-05-20 13:21:35 -0500

    Subida inicial de backend

---
+++ manofacil/src/main/java/com/manofacil/manofacil/ManofacilApplication.java
@@ -0,0 +1,13 @@
+package com.manofacil.manofacil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManofacilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManofacilApplication.class, args);
+	}
+
+}
---
+++ manofacil/src/test/java/com/manofacil/manofacil/TestManofacilApplication.java
@@ -0,0 +1,11 @@
+package com.manofacil.manofacil;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestManofacilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(ManofacilApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
